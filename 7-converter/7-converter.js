2880fb3e738385a0fd06d6bd8abe910bf8853af5
YanaAvetisyan <yanagithuby@gmail.com>
2025-03-13 13:16:30 +0300

    update 7-convertver

--- 7-converter/7-converter.js
+++ 7-converter/7-converter.js
@@ -1,17 +1,44 @@
-// 7-converter
 function convertation(sum, myCurrency, newCurrency) {
-
+    let rate = getExchangeRate(myCurrency, newCurrency);
+    return rate !== null ? sum * rate : null;
 }
 
-function checkCurrency(newCurrency) {
-    switch (newCurrency) {
-        case 'eur':
-            return 97.75;
+function getExchangeRate(from, to) {
+    switch (from) {
+        case 'rub':
+            switch (to) {
+                case 'usd': return 1 / 93.75;
+                case 'eur': return 1 / 97.75;
+                case 'cny': return 1 / 12.59;
+            }
+            break;
         case 'usd':
-            return 93.75;
+            switch (to) {
+                case 'rub': return 93.75;
+                case 'eur': return 93.75 / 97.75;
+                case 'cny': return 93.75 / 12.59;
+            }
+            break;
+        case 'eur':
+            switch (to) {
+                case 'rub': return 97.75;
+                case 'usd': return 97.75 / 93.75;
+                case 'cny': return 97.75 / 12.59;
+            }
+            break;
         case 'cny':
-            return 12.59;
-        default:
-            return null;
+            switch (to) {
+                case 'rub': return 12.59;
+                case 'usd': return 12.59 / 93.75;
+                case 'eur': return 12.59 / 97.75;
+            }
+            break;
     }
+    return null;
 }
+
+console.log(convertation(1000, 'rub', 'usd'));
+console.log(convertation(50, 'usd', 'eur'));
+console.log(convertation(100, 'cny', 'rub'));
+console.log(convertation(100, 'eur', 'cny'));
+console.log(convertation(100, 'usd', 'jpy')); 
